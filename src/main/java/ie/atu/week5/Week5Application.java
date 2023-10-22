54fac5a8c7bf80417d51ca60d9e1decaa8b1ae7d
NathanPerry057 <G00394355@atu.ie>
2023-10-22 18:31:41 +0100

    Added main class with the rest template

---
+++ src/main/java/ie/atu/week5/Week5Application.java
@@ -0,0 +1,19 @@
+package ie.atu.week5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Week5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {return new RestTemplate();}
+}
